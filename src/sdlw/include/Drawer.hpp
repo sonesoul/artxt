b64e2a1560d50ce151568e3855d32165a1bac131
sonesoul <smippy098@gmail.com>
2025-10-26 19:44:29 -0600

    add Renderer and Drawer classes for rendering

---
+++ src/sdlw/include/Drawer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "SDL3/SDL.h"
+#include "Color.hpp"
+
+namespace sdlw {
+	
+	struct Drawer {
+
+	private:
+		SDL_Renderer* const _renderer;
+
+	public: 
+
+		Drawer(const Drawer&) = delete;
+
+		inline Drawer(SDL_Renderer* renderer) : 
+			_renderer(renderer) { }
+
+		inline void setColor(const Color& c) const {
+			SDL_SetRenderDrawColor(_renderer, c.R, c.G, c.B, c.A);
+		}
+		inline SDL_Renderer* getRenderer() const {
+			return _renderer;
+		}
+
+		Drawer& operator=(const Drawer&) = delete;
+	};
+}
--- src/sdlw/include/Renderer.hpp
+++ src/sdlw/include/Renderer.hpp
@@ -1,12 +1,29 @@
 #pragma once 
 #include "SDL3/SDL.h"
+#include "Event.hpp"
+#include "Drawer.hpp"
 
 namespace sdlw {
-	
+
 	class Renderer {
 
-	public: 
-		Renderer();
+	private:
 
+		SDL_Renderer* _target;
+		int _delayTimeMs;
+		sdlw::Event<Drawer&> _event;
+
+	public:
+		Renderer(SDL_Window* window);
+
+		void Render();
+		void setTargetFPS(int fps);
+
+		inline sdlw::Event<Drawer&>& event() {
+			return _event;
+		}
+		inline SDL_Renderer* target() const {
+			return _target;
+		}
 	};
 }
