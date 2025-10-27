3b20f00917936cb6b6def41792a5fc4aea3d9199
sonesoul <smippy098@gmail.com>
2025-10-26 19:47:42 -0600

    implement window events using Event class

--- src/sdlw/Window.cpp
+++ src/sdlw/Window.cpp
@@ -1,53 +1,25 @@
-#include "sdlw/include/Window.hpp";
-#include "sdlw/include/Renderer.hpp"
+#include "include/Window.hpp"
+#include "include/Renderer.hpp"
 
-#include <unordered_map>
-#include <vector>
-#include <functional>
+using namespace sdlw;
 
-namespace sdlw {
+Window::Window(int width, int height, const char* title, SDL_WindowFlags flags) :
+	_target(SDL_CreateWindow(title, width, height, flags)),
+	_sdlEvent(SDL_Event()) { }
 
-	Window::Window(int width, int height, const char* title, SDL_WindowFlags flags) {
+Window::~Window() {
+	SDL_DestroyWindow(_target);
+}
 
-		_window = SDL_CreateWindow(title, width, height, flags);
-		_event = SDL_Event();
-	}
+void Window::PollEvents() {
 
-	Window::~Window() {
-		SDL_DestroyWindow(_window);
-	}
+	SDL_Event* sdlEventPtr = &_sdlEvent;
+	while (SDL_PollEvent(sdlEventPtr)) {
 
-	const SDL_Window* Window::getWindow() const {
-		return _window;
-	}
+		auto it = _eventMap.find(sdlEventPtr->type);
 
-	Callback* Window::CreateCallback(Uint32 eventType, Callback::fnType function) {
-		
-		Callback* callback = new Callback(eventType, function);
-		_eventMap[eventType].push_back(callback);
-		return callback;
-	}
-
-	void Window::DeleteCallback(Callback* callback) {
-		auto& vec = _eventMap[callback->getEventType()];
-		
-		for (auto it = vec.begin(); it != vec.end(); it++) {
-
-			if (callback == *it) {
-				vec.erase(it);
-				delete callback;
-				return;
-			}
-		}
-	}
-
-	void Window::PollEvents() {
-		while (SDL_PollEvent(&_event)) {
-			auto& vec = _eventMap[_event.type];
-
-			for (auto& it : vec) {
-				it->Invoke(&_event);
-			}
+		if (it != _eventMap.end()) {
+			it->second.invoke(sdlEventPtr);
 		}
 	}
 }
