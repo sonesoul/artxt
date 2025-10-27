8213beccc01425b57c1d91ca412db8b2f368f7e6
sonesoul <smippy098@gmail.com>
2025-10-26 19:44:29 -0600

    implement rendering architecture

---
+++ src/sdlw/Renderer.cpp
@@ -0,0 +1,24 @@
+#include "include/Renderer.hpp"
+
+#include "SDL3/SDL.h"
+#include "SDL3_image/SDL_image.h"
+#include "SDL3_ttf/SDL_ttf.h"
+
+using namespace sdlw;
+
+Renderer::Renderer(SDL_Window* window) : 
+	_target(SDL_CreateRenderer(window, nullptr)), 
+	_delayTimeMs(1) { }
+
+void Renderer::setTargetFPS(int fps) {
+	_delayTimeMs = 1000 / fps;
+}
+
+void Renderer::Render() {
+	SDL_RenderClear(_target);
+
+	Drawer d(_target);
+	_event.invoke(d);
+
+	SDL_RenderPresent(_target);
+}
