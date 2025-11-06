760a31419622d6a5fa393fff734b3ee268fa72e9
sonesoul <smippy098@gmail.com>
2025-11-05 20:10:03 -0700

    add Texture wrap header

---
+++ include/sdlw/Texture.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "SDL3/SDL.h"
+
+namespace sdlw {
+	class Texture {
+
+		SDL_Texture* _texture;
+
+	public: 
+		Texture(SDL_Texture* texture) : 
+			_texture(texture) {
+		}
+		Texture(SDL_Renderer* renderer, SDL_Surface* surface) :
+			Texture(SDL_CreateTextureFromSurface(renderer, surface)) {
+		}
+		~Texture() {
+			SDL_DestroyTexture(_texture);
+		}
+
+		inline void SetScaleMode(SDL_ScaleMode scaleMode) {
+			SDL_SetTextureScaleMode(_texture, scaleMode);
+		}
+
+		inline SDL_Texture* const target() const {
+			return _texture;
+		}
+	};
+}
