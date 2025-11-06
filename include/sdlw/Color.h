daf548b19ab1cdfff2cdf3f9c44ec27aabf74cad
sonesoul <smippy098@gmail.com>
2025-11-05 22:16:33 -0700

    move implementation to header

--- include/sdlw/Color.h
+++ include/sdlw/Color.h
@@ -7,10 +7,17 @@ namespace sdlw {
 	struct Color {
 		byte R, G, B, A;
 
-		Color(byte r, byte g, byte b, byte a);
-		Color(byte r, byte g, byte b);
-		Color();
-
-		operator SDL_Color() const;
+		Color(byte r, byte g, byte b, byte a) : 
+			R(r), G(g), B(b), A(a) {
+		}
+		Color(byte r, byte g, byte b) :
+			Color(r, g, b, 255) {
+		}
+		Color() :
+			Color(0, 0, 0, 0) {
+		}
+		inline operator SDL_Color() const {
+			return SDL_Color{ R, G, B, A };
+		}
 	};
 }
