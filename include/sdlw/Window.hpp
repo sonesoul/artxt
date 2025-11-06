fab0266e1c9e3560da051cf7edf8cd8f4b6c520c
sonesoul <smippy098@gmail.com>
2025-11-05 21:47:10 -0700

    rename _target to _window and replace char* with string

--- include/sdlw/Window.hpp
+++ include/sdlw/Window.hpp
@@ -1,21 +1,21 @@
 #pragma once
-#include "SDL3/SDL.h"
 #include <unordered_map>
+#include <string>
+#include "SDL3/SDL.h"
 #include "Event.hpp"
 
 namespace sdlw {
-	
 	class Window {
 		
 	private:
 
-		SDL_Window* _target;
+		SDL_Window* _window;
 
 		SDL_Event _sdlEvent;
 		std::unordered_map<Uint32, Event<const SDL_Event*>> _eventMap;
 
 	public:
-		Window(int width, int height, const char* title, SDL_WindowFlags flags);
+		Window(int width, int height, const std::string& title, SDL_WindowFlags flags);
 		~Window();
 		
 		void PollEvents();
@@ -24,7 +24,7 @@ namespace sdlw {
 			return _eventMap[sdlEventType];
 		}
 		inline SDL_Window* target() const {
-			return _target;
+			return _window;
 		}
 	};
 }
