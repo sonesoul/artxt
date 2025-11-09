65d9414aa7eaf5c7391cae083af3a9cd4e656a54
sonesoul <smippy098@gmail.com>
2025-11-08 23:33:22 -0700

    fix calculations

--- include/sdlw/Time.h
+++ include/sdlw/Time.h
@@ -2,31 +2,26 @@
 #include "SDL3/SDL.h"
 
 static double TARGET_FPS = 60;
-static double TARGET_FRAME_TIME = 1000 / TARGET_FPS;
+static Uint64 TARGET_FRAME_TIME = 1000 / TARGET_FPS;
 
 static Uint64 _frameStart = 0;
-static Uint64 _lastFrame = 0;
 
 static double _deltaTime = 0;
 
-void FrameTimeStart() {
-	Uint64 ms = ticks();
+void Time_FrameStart() {
+	Uint64 ms = SDL_GetTicks();
 
-	_deltaTime = 1.0 / ms - _lastFrame;
-	_lastFrame = ms;
+	_deltaTime = (ms - _frameStart) / 1000.0;
+	_frameStart = ms;
 }
-void FrameTimeEnd() {
-	Uint64 frameTime = ticks() - _frameStart;
+void Time_FrameEnd() {
+	Uint64 frameTime = SDL_GetTicks() - _frameStart;
 
 	if (frameTime < TARGET_FRAME_TIME) {
 		SDL_Delay(TARGET_FRAME_TIME - frameTime);
 	}
 }
 
-inline double deltaTime() {
+inline double Time_delta() {
 	return _deltaTime;
-}
-
-inline Uint64 ticks() {
-	return SDL_GetTicks();
 }
