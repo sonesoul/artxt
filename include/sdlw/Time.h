b6f37d1f7cf5129fbd715b43ee37f1f39f03674e
sonesoul <smippy098@gmail.com>
2025-11-13 00:35:28 -0700

    wait logic enhancement

--- include/sdlw/Time.h
+++ include/sdlw/Time.h
@@ -5,21 +5,32 @@ namespace sdlw {
 
 	class Time {
 
+	public:
 		static constexpr Uint16 TARGET_FPS = 60;
 		static constexpr double TARGET_FRAME_TIME_MS = 1000.0 / TARGET_FPS;
+		static constexpr double TARGET_FRAME_TIME_S = 1.0 / TARGET_FPS;
 
+	private:
 		static Uint64 _lastTime;
 		static Uint64 _frequency;
 		static double _deltaTime;
 
 	public:
-
 		Time() = delete;
 
 		static void Update();
 
 		static inline double delta() {
 			return _deltaTime;
+		}
+		
+	private:
+		static inline double getFrameTime(Uint64 counter) {
+			return (double)(counter - _lastTime) / _frequency;
+		}
+
+		static inline Uint64 getCounter() {
+			return SDL_GetPerformanceCounter();
 		}
 	};
 }
