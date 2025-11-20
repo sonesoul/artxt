b18e3475392e5fa97530e9480d9b0af7c75374d6
sonesoul <smippy098@gmail.com>
2025-11-19 21:53:31 -0700

    prevent copying to avoid double deletion; add move semantics

--- include/sdlw/Surface.h
+++ include/sdlw/Surface.h
@@ -9,6 +9,24 @@ namespace sdlw {
 		Surface(SDL_Surface* surface) : 
 			_surface(surface) { 
 		}
+
+		Surface(Surface&) = delete;
+		Surface(Surface&& other) noexcept : 
+			_surface(other._surface) {
+			other._surface = nullptr;
+		}
+
+		Surface& operator=(Surface&) = delete;
+		Surface& operator=(Surface&& other) noexcept {
+			if (this != &other) {
+				SDL_DestroySurface(_surface);
+				_surface = other._surface;
+				other._surface = nullptr;
+			}
+
+			return *this;
+		}
+
 		~Surface() {
 			SDL_DestroySurface(_surface);
 		}
