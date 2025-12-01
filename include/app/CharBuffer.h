9c47610ac65d3bfd625e4cdefac27ea60d394471
sonesoul <smippy098@gmail.com>
2025-11-30 22:30:08 -0700

    add CharBuffer

---
+++ include/app/CharBuffer.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <memory>
+
+class CharBuffer {
+	
+private:
+	uint8_t _width, _height;
+	uint16_t _length;
+	std::unique_ptr<char[]> _buffer;
+	
+public: 
+	CharBuffer(uint8_t w, uint8_t h) :
+		_width(w),
+		_height(h),
+		_length(uint16_t(w)* h),
+		_buffer(std::make_unique<char[]>(_length)) {
+		Fill(' ');
+	}
+
+	inline void Fill(char c) {
+		std::memset(_buffer.get(), c, _length);
+	}
+
+	inline char& at(uint8_t x, uint8_t y) {
+		return _buffer[uint16_t(y) * _width + x];
+	}
+
+	inline uint16_t length() const {
+		return _length;
+	}	
+};
